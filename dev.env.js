9e2cdef006b141d6cc72a80fc5cbdf5867b4fdfd
Ninh Chu <ninhchu@binvest.vn>
2021-03-21 14:30:34 +0000

    move env file into serperate files

--- dev.env.js
+++ dev.env.js
@@ -12,10 +12,14 @@ module.exports = {
     // If these are your db settings then you don't need to be explicit. If not
     // you can uncomment this and get more specific.
     'apostrophe-db': {
-      // uri: 'mongodb://localhost:27017/apostrophe-sandbox'
+      uri: 'mongodb://ninhchu:Ninh0909!@ninhchu.ddns.net:9992/FidtWebiste?authSource=admin'
       // There is legacy support for host, port, name, user and password options,
       // but this is not necessary. They can all go in the uri option like this:
       // mongodb://user:password@host:port/dbname
+    },
+    'apostrophe-express': {
+      // port: 3333
     }
   }
 };
+
