c3403b9d903b8b8eb1e3b5735c7e2e063eaf5e9a
Ninh Chu <ninhchu@binvest.vn>
2021-04-01 23:01:43 +0000

    add favicon

--- settings/headerNavigations.js
+++ settings/headerNavigations.js
@@ -5,67 +5,28 @@ module.exports = [
         url: "/#about-us"
     },
     {
-        type: "dropdown",
+        type: "single",
         title: "Tư vấn đầu tư",
-        menuItems: [
-            {
-                title: "Đầu tư cổ phiếu toàn diện",
-                url: "/consulting/stocks/cci"
-            },
-            {
-                title: "Đầu tư ngắn hạn",
-                url: "/consulting/stocks/short-term-trading"
-            }
-        ]
+        url: "#"
     },
     {
-        type: "dropdown",
+        type: "single",
         title: "Quản lý tài sản",
-        menuItems: [
-            {
-                title: "Quản lý danh mục",
-                url: "/management/apm"
-            },
-            {
-                title: "Quản lý đầu tư BĐS",
-                url: "/management/real-estate"
-            },
-            {
-                title: "Quản lý tài sản cá nhân",
-                url: "/management/wealth-management"
-            },
-            {
-                title: "Quỹ đầu tư",
-                url: "/management/fund"
-            }
-        ]
+        url: "#"
     },
     {
-        type: "dropdown",
+        type: "single",
         title: "Đào tạo",
-        menuItems: [
-            {
-                title: "Kỹ năng đầu tư",
-                url: "/training/investment"
-            },
-            {
-                title: "Quản lý tài chính cá nhân",
-                url: "/training/sell-managed-finance"
-            }
-        ]
+        url: "#"
     },
     {
-        type: "dropdown",
-        title: "Tiêu điểm đầu tư",
-        menuItems: [
-            {
-                title: "Blog đầu tư",
-                url: "/spotlight/blog"
-            },
-            {
-                title: "Ấn phẩm đầu tư",
-                url: "/spotlight/investment-reports"
-            }
-        ]
+        type: "single",
+        title: "Ấn phẩm đầu tư",
+        url: "/investment-reports"
+    },
+    {
+        type: "single",
+        title: "Blog",
+        url: "/blog"
     }
 ]
